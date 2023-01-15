7f2719c96f0d7825185444ec732afd6f0df25053
LACHIRI Ilias <ilias.lachiri@grenoble-inp.org>
2023-01-15 23:45:05 +0100

    Script de JACOCO

---
+++ test_context.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+DEBUT=$(date +%s.%N)
+
+for i in src/test/deca/context/valid/provided/*.deca
+do
+echo "$i"
+
+
+
+test_context "$i" > "${i%.deca}".lis
+
+
+if [ $? -ne 0 ]
+then
+    echo  "\033[0;31mTEST CONTEXT ERREUR\033[0m"
+else
+    echo  "\033[0;32mTEST CONTEXT PASSÉ \033[0m"
+fi
+
+
+done
+
+FIN=$(date +%s.%N)
+
+echo "TEMPS D'EXECUTION TOTAL DES TESTS  : $(echo "$FIN - $DEBUT" | bc) secondes"
+
