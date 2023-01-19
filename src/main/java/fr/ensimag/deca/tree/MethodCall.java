697b6f4cbb70f10a8541fd56713f368fd9d57b69
LACHIRI Ilias <ilias.lachiri@grenoble-inp.org>
2023-01-19 19:31:26 +0100

    Avec Objet etape C : Ajout p.methode

--- src/main/java/fr/ensimag/deca/tree/MethodCall.java
+++ src/main/java/fr/ensimag/deca/tree/MethodCall.java
@@ -6,8 +6,21 @@ import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.tools.IndentPrintStream;
+import fr.ensimag.ima.pseudocode.ImmediateInteger;
 import fr.ensimag.ima.pseudocode.ImmediateString;
+import fr.ensimag.ima.pseudocode.Label;
+import fr.ensimag.ima.pseudocode.NullOperand;
+import fr.ensimag.ima.pseudocode.Register;
+import fr.ensimag.ima.pseudocode.RegisterOffset;
+import fr.ensimag.ima.pseudocode.instructions.ADDSP;
+import fr.ensimag.ima.pseudocode.instructions.BEQ;
+import fr.ensimag.ima.pseudocode.instructions.BSR;
+import fr.ensimag.ima.pseudocode.instructions.LOAD;
+import fr.ensimag.ima.pseudocode.instructions.STORE;
+import fr.ensimag.ima.pseudocode.instructions.SUBSP;
 import fr.ensimag.ima.pseudocode.instructions.WSTR;
+
+
 import java.io.PrintStream;
 import org.apache.commons.lang.Validate;
 import fr.ensimag.deca.context.StringType;
@@ -47,9 +60,39 @@ public class MethodCall extends AbstractExpr{
 
     @Override
     protected void codeGenInst(DecacCompiler compiler) {
-        //p14
-        throw new UnsupportedOperationException("not yet implemented");
+        //p12
+        compiler.addInstruction(new ADDSP(new ImmediateInteger(1+lstExpr.size())));
+        //compiler.addInstruction(new LOAD(,Register.getR(compiler.getRegisterAllocator().newRegister(compiler))));
+
+        compiler.addInstruction(new STORE(Register.getR(compiler.getRegisterAllocator().popRegister()),new RegisterOffset(0, Register.SP)));
+
+        // DAddr dr = expr.getExpDefinition().getOperand() // Code gen qui pemet d'appeler getExpe
+
+         // For les parametre !!!! 
+
+         for(int i=0;i<lstExpr.size();i++){
+            //Ajouter codeGen adresse // popRegister=> on prend la derniere valeur du registre !!!!!!
+            //compiler.addInstruction(new LOAD(Register.getR(lstExpr.getIndex(i).,Register.getR(compiler.getRegisterAllocator().popRegister()))));
+            compiler.addInstruction(new STORE(Register.getR(compiler.getRegisterAllocator().popRegister()),new RegisterOffset(-1-i, Register.SP)));
+         }
+       
+
+       
+        compiler.addInstruction(new LOAD(new RegisterOffset(0, Register.SP),Register.getR(compiler.getRegisterAllocator().popRegister())));
+        compiler.addInstruction(new LOAD(new NullOperand(),(Register.getR(compiler.getRegisterAllocator().popRegister()))));
+      
+      //  compiler.addInstruction(new BEQ(new Label("dereferncement_null")));
+        
+        compiler.addInstruction(new LOAD(new RegisterOffset(0,Register.getR(compiler.getRegisterAllocator().popRegister())) ,(Register.getR(compiler.getRegisterAllocator().popRegister()))));
+        compiler.addInstruction(new BSR(new RegisterOffset(2,Register.getR(compiler.getRegisterAllocator().popRegister())) )); // OFFSET !!!!!!
+        compiler.addInstruction(new SUBSP(new ImmediateInteger(1+lstExpr.size())));
+        
+        
+    
+    
+    
     }
+    
 
     @Override
     public void decompile(IndentPrintStream s) {
